705ddcf14ad979f226d62d7dad8917b9c561419e
pndayo1404 <tranvanphucnguyen12@gmail.com>
2022-11-21 23:17:46 +1100

    Finish first game protoype

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,77 @@
 console.log("Hello world");
-function getComputerChoice(){
+
+function getComputerChoice(computerChoice){
     let choice = Math.floor(Math.random() * 3);
-    let computerChoice = "";
+    
     if (choice == 0){
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     else if (choice == 1){
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     else if (choice == 2){
-        computerChoice = "Scizzors";
+        computerChoice = "scizzors";
+    }
+    return computerChoice;
+}
+
+function getPlayerChoice(){
+    let playerChoice = prompt("Enter your choice: ").toLowerCase();
+    return playerChoice;
+}
+
+function decideWinner(computerChoice, playerChoice){
+    if (computerChoice == "rock"){
+        if (playerChoice == "rock"){
+            result = "draw";
+        }
+        else if (playerChoice == "paper"){
+            result = "you win";
+        }
+        else if (playerChoice == "scizzors"){
+            result = "you lose";
+        }
+    }
+    else if (computerChoice == "paper"){
+        if (playerChoice == "rock"){
+            result = "you lose";
+        }
+        else if (playerChoice == "paper"){
+            result = "draw";
+        }
+        else if (playerChoice == "scizzors"){
+            result = "you win";
+        }
+    }
+    else if (computerChoice == "scizzors"){
+        if (playerChoice == "rock"){
+            result = "you win";
+        }
+        else if (playerChoice == "paper"){
+            result = "you lose";
+        }
+        else if (playerChoice == "scizzors"){
+            result = "draw";
+        }
+    }
+    return result;
+}
+
+function game(){
+    let computerChoice = "";
+    let result = "";
+    computerChoice = getComputerChoice(computerChoice);
+    console.log(computerChoice);
+    let playerChoice = getPlayerChoice();
+    console.log(playerChoice);
+    result = decideWinner(computerChoice, playerChoice);
+    console.log(result);
+}
+
+function loopGame(){
+    for (let i = 0; i<5; i++){
+        game();
     }
 }
+
+loopGame();
