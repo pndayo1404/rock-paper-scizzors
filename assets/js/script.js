6fbd0f04e16c7d03c0315e36247e3c716a0c16fc
pndayo1404 <tranvanphucnguyen12@gmail.com>
2022-11-21 22:43:31 +1100

    Write function getComputerChoice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 console.log("Hello world");
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+    if (choice == 0){
+        computerChoice = "Rock";
+    }
+    else if (choice == 1){
+        computerChoice = "Paper";
+    }
+    else if (choice == 2){
+        computerChoice = "Scizzors";
+    }
+}
