4ca9a14f10966e24ab20c10d83ace6f495c3cff2
pndesu <tranvanphucnguyen1@gmail.com>
2022-11-28 14:00:18 +1100

    Add css files and several js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,13 +53,62 @@ function decideWinner(computerChoice, playerChoice){ //Decide winner with if els
     return result;
 }
 
+let countPlayer = 0, countComputer = 0;
+const playerScore = document.querySelector('.playerScore');
+const computerScore = document.querySelector('.computerScore');
+function countScore(result){ 
+    if (result == 'you win'){
+        countPlayer += 1;
+        // playerScore.textContent = "You:  " + countPlayer + " vs ";
+        // displayScore();
+    }
+    else if (result == 'you lose'){
+        countComputer += 1;
+        // computerScore.textContent = "Computer:  " + countComputer;
+        // displayScore();
+    }
+}
+const displayChoice = document.querySelector('.displayChoice');
+function displayScore(playerChoice, computerChoice){
+    playerScore.textContent = "You:  " + countPlayer + " vs ";
+    computerScore.textContent = "Computer:  " + countComputer;
+    displayChoice.textContent = playerChoice + " vs " + computerChoice;
+}
+const winner = document.querySelector('.matchWinner');
+function matchWinner(countPlayer, countComputer){ //Display match winner when score reaches 3
+    if (countPlayer == 3){
+        winner.textContent += "You won the match!";
+        resetGame();
+    }
+    else if (countComputer == 3){
+        winner.textContent += "Computer won the match!";
+        resetGame();
+    }
+}
+const resetButton = document.querySelector('.resetGame');
+function resetGame(){ 
+    resetButton.style.display = 'block';
+    resetButton.addEventListener('click', function(e){
+        countPlayer = 0;
+        countComputer = 0;
+        playerScore.textContent = "You:  " + countPlayer + " vs ";
+        computerScore.textContent = "Computer:  " + countComputer;
+        winner.textContent = '';
+        displayChoice.textContent = '';
+        resetButton.style.display = 'none';
+    })
+}
 function game(playerChoice){ //Play a game with player's input
+    if (countPlayer >= 3 || countComputer >= 3) return;
     let computerChoice = "";
     let result;
     computerChoice = getComputerChoice(computerChoice);
     console.log(computerChoice);
     result = decideWinner(computerChoice, playerChoice);
     console.log(result);
+    countScore(result);
+    displayScore(playerChoice, computerChoice);
+    matchWinner(countPlayer, countComputer);
 }
 
 const buttons = document.querySelectorAll('button');
